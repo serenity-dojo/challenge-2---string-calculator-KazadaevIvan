0e6bd9b783db67a4fb38893c4901d41cd695cfd2
Ivan Kazadaew <ivan.kazadaew@leverx.com>
2023-02-24 12:09:37 +0300

    Complete te challenge

--- src/main/java/com/serenitydojo/calculator/Calculator.java
+++ src/main/java/com/serenitydojo/calculator/Calculator.java
@@ -1,7 +1,38 @@
 package com.serenitydojo.calculator;
 
+import java.util.Arrays;
+
 public class Calculator {
-    public int evaluate(String expression) {
-        return 0;
+    public int evaluate(String expression) throws IllegalMathsOperatorException {
+        if (expression.equals("")) {
+            return 0;
+        } else {
+            String[] numbers = expression.split("\\D+");
+            String[] operators = expression.split("[0-9]+");
+            int result = Integer.parseInt(numbers[0].trim());
+            for (int i = 1; i < numbers.length; i++) {
+                switch (operators[i].trim()) {
+                    case "+":
+                        result += getIntegerFromString(numbers[i]);
+                        break;
+                    case "-":
+                        result -= getIntegerFromString(numbers[i]);
+                        break;
+                    case "*":
+                        result *= getIntegerFromString(numbers[i]);
+                        break;
+                    case "/":
+                        result /= getIntegerFromString(numbers[i]);
+                        break;
+                    default:
+                        throw new IllegalMathsOperatorException(String.format("%s operator can't be used here", operators[i].trim()));
+                }
+            }
+            return result;
+        }
+    }
+
+    public int getIntegerFromString(String string) {
+        return Integer.parseInt(string.trim());
     }
 }
